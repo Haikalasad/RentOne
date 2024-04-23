c9bdd01c39ba64997dd68f26e2c3122b1e17e8fa
Haikalasad <muhammad.haikal.21@student.if.ittelkom-sby.ac.id>
2024-04-23 20:24:39 +0700

    Dashboard RentOne

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,188 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap">
+  <link rel="stylesheet" href="dashboard.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/fadd57a8f9.js" crossorigin="anonymous"></script>
 </head>
+</head>
+
 <body>
-    <h1>Ini halaman dashboard</h1>
+
+  <!-- navbar section -->
+
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+     <h2>RentOne</h2>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+
+
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+        </ul>
+
+        <button class="btn btn-primary" type="submit" style="width: 100px;   background-color: #053CC9;">Book Now</button>
+      </div>
+    </div>
+
+  </nav>
+
+
+  <!-- hero section -->
+
+  <section id="hero-section">
+    <div class="row justify-content-center m-5">
+      <div class="col-lg-6 p-0 col-md-3 mt-4 align-content-center mx-auto">
+        <h1>
+          UNLOCK YOUR TRAVEL EXPERIENCE
+        </h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque sodales ut etiam sit amet nisl purus.
+        </p>
+        
+        <button class="btn btn-primary" type="submit">Get started</button>
+        
+      </div>
+
+      <div class="col-lg-4 col-md-2">
+        <img src="asset/Hero.avif" width="400px" />
+      </div>
+    </div>
+
+    
+    </div>
+
+  </section>
+
+
+
+
+
+  
+
+
+
+
+  <!-- Available cars section -->
+
+
+  <section id="live_event">
+    <div class="row justify-content-center ">
+      <div class="title">
+        <h1 style="text-align: center; margin-top : 70px;margin-bottom :10px ; font-weight : 700; font-size : 40px;">Availabale cars</h1>
+        <p style="text-align: center; margin-bottom : 70px">Book your car fast!!</p>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card" style="width: 18rem;">
+          <img src="https://images.unsplash.com/photo-1541443131876-44b03de101c5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fENhcnxlbnwwfHwwfHx8MA%3D%3D" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">BMW I7 2023</h5>
+            <p class="card-text">BMW INDONESIA</p>
+            <p class="card-text "><small class="text-body-secondary">Available</small></p>
+            <a href="#" class="btn btn-primary">Hubungi kami</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card" style="width: 18rem;">
+          <img src="https://images.unsplash.com/photo-1590362891991-f776e747a588?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Mercedez Benz New GLA</h5>
+            <p class="card-text">Mercedez Benz Indonesia</p>
+            <p class="card-text "><small class="text-body-secondary">Available</small></p>
+            <a href="#" class="btn btn-primary">Hubungi kami</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card" style="width: 18rem;">
+          <img src="https://images.unsplash.com/photo-1542228262-3d663b306a53?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Nissan Type GT-R</h5>
+            <p class="card-text">Nissan Indonesia</p>
+            <p class="card-text "><small class="text-body-secondary">Available</small></p>
+            <a href="#" class="btn btn-primary">Hubungi kami</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+   <!-- Logo partner section -->
+
+   <section id="logo-section" class="p-5">
+   <h1 style="text-align: center; margin-top : 70px;margin-bottom :50px ; font-weight : 700; font-size : 40px;">Our Partner</h1>
+    <div class="container2  ms-5 p-3">
+    
+      <div class="row justify-content-center ">
+
+        <div class="col-lg-2 col-md-1 align-content-center">
+          <img src="asset/toyota.png" alt="Toyoya Logo" width="150px">
+        </div>
+        <div class="col-lg-2 col-md-1 align-content-center">
+          <img src="https://th.bing.com/th/id/OIP.eQtWp3DaISrvEH8bQ3Bh4gHaFQ?w=241&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7" alt="BMW Logo" width="100px">
+        </div>
+      
+
+        <div class="col-lg-2 col-md-1 align-content-center">
+          <img src="https://seeklogo.com/images/H/honda-silver-logo-E810C6A8A4-seeklogo.com.png" alt="Honda Logo" width="100px">
+        </div>
+      
+        <div class="col-lg-2 col-md-1 align-content-center">
+          <img src="https://logodownload.org/wp-content/uploads/2014/04/mercedes-benz-logo-8.png" alt="Mercedez Logo" width="90px">
+        </div>
+      
+      
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+  <footer>
+
+    <div class="row justify-content-center" style="background-color: #000033;">
+      <div class="col-md-3 m-4 align-content-center mx-auto">
+        <h2 style ="color :#ffffff;">RentOne</h2>
+        <p style="color: white; margin-top:20px;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br>Neque sodales ut etiam sit amet nisl purus..</p>
+      </div>
+
+      <div class="col-md-4 m-5  align-content-center" style="color: white;">
+        <h2 style="margin-bottom: 20px;">Contact Info</h2>
+
+        <p> <i class="fa-solid fa-location-dot" style="color: #ffffff; margin-right : 10px;"></i>Jl. Ketintang No.156, Ketintang, <br>Kec. Gayungan, Surabaya, Jawa Timur 60231</p>
+        <p> <i class="fa-solid fa-phone" style="color: #ffffff;margin-right : 10px;"></i>(330)7891-5568</p>
+        <p> <i class="fa-solid fa-envelope" style="color: #ffffff; margin-right : 10px;"></i>RentOne@gmail.com</p>
+      </div>
+    </div>
+  </footer>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
